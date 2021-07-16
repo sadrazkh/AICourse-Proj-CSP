0c04b2cdcdb63bab93fe70d1f18bfceff6a66fd2
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2021-07-16 10:54:29 +0430

    Add project files.

---
+++ AICourse-Proj-CSP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AICourse_Proj_CSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
